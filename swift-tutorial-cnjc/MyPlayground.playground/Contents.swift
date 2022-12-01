122dac153aa802e513cd4408af648e5de449f601
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-01 23:33:54 +0900

    cnjc: learn "keywords, types"

--- swift-tutorial-cnjc/MyPlayground.playground/Contents.swift
+++ swift-tutorial-cnjc/MyPlayground.playground/Contents.swift
@@ -1,11 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting: String;
+greeting = "Hello, playground" // semicolon(;) is not necessary
+greeting = "Hello, playground" // or add semicolon(;) at end of code
+
+// need semicolon(;)
+var hw = "Hello, World!"; print(hw)
 
 
-var hw = "Hello, World!";
-print(hw)
 
+/* ------------------------------------ comment ------------------------------------ */
 // one line comment
 
 /* multiple
@@ -16,5 +20,55 @@ print(hw)
 /* another comment */
    end of comment */
 
-// need semicolon(;)
-var a = "Hello, World!"; print(a)
+
+
+/* ------------------------------------ keyword ------------------------------------ */
+/*
+ => declare keyword:
+ class    deinit     enum     extension   func        import
+ init     internal   let      operator    private     protocol
+ public   static     struct   subscript   typealias   var
+ */
+/*
+ => grammar keyword:
+ break   case   continue   default   do       else    fallthrough
+ for     if     in         return    switch   where   while
+ */
+/*
+ => expression & type keyword:
+ as      dynamicType   false      is       nil         self     Self
+ super   true          _COLUMN_   _FILE_   _FUNCTION_  _LINE_
+ */
+/*
+ => special keyword:
+ associativity convenience dynamic didSet final get inifix inout lazy left mutating none
+ nonmutating optional override postfix precedence prefix Protocol required right set Type
+ unowned weak willSet
+ */
+
+
+
+/* ------------------------------------ space ------------------------------------ */
+// let a= 1 + 2 => error: '=' must have consistent whitespace on both sides
+// let a = 1+ 2 => error: '+' is not a postfix unary operator
+let a = 1 + 2 // => right
+let b = 3+4 // => alse right
+
+
+
+/* ------------------------------------ variable type ------------------------------------ */
+let int = 42;                 // int
+let float = 3.1315926;        // float
+let string = "Hello, World!"; // string
+let boolean = true;           // boolean
+
+
+
+/* ------------------------------------ print ------------------------------------ */
+print("Hello, World!");
+
+for x in 0...10 {
+    print("\(x)", terminator: " ");
+}
+
+let theInput = readLine();
