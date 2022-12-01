508c4d2b52dc276b20174a3588f76a3e0eab99bc
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-01 21:09:05 +0900

    cnjc: first commit

---
+++ swift-tutorial-cnjc/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+var hw = "Hello, World!";
+print(hw)
+
+// one line comment
+
+/* multiple
+   line
+   comment */
+
+/* start of comment
+/* another comment */
+   end of comment */
+
+// need semicolon(;)
+var a = "Hello, World!"; print(a)
