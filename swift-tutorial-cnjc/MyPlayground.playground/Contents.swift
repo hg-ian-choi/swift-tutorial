f35fac8eb8dee679330994d8111839c2ae85d896
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-02 23:16:03 +0900

    cnjc: add type range

--- swift-tutorial-cnjc/MyPlayground.playground/Contents.swift
+++ swift-tutorial-cnjc/MyPlayground.playground/Contents.swift
@@ -57,14 +57,27 @@ let b = 3+4 // => alse right
 
 
 /* ------------------------------------ variable type ------------------------------------ */
-let int:Int = -42;                   // Integer (int)
-let uint:UInt = 42;                  // Unsigned Integer (uint)
-let float:Float = 3.14;              // float
-let double:Double = 3.1415926;       // double
-let string:String = "Hello, World!"; // string
-let bool:Bool = true;                // Boolean
-let character: Character = "C";      // character (char)
-let optional:Optional = {};          // Optional (include null and undefined)
+let int :Int = -42;                   // Integer (int)
+let uint :UInt = 42;                  // Unsigned Integer (uint)
+let float :Float = 3.14;              // float
+let double :Double = 3.1415926;       // double
+let string :String = "Hello, World!"; // string
+let bool :Bool = true;                // Boolean
+let character :Character = "C";       // character (char)
+let optional :Optional = {};          // Optional (include null and undefined)
+
+
+
+/* ------------------------------------ range ------------------------------------ */
+let int8_range :Int8 = -128;                     // 1 byte => -128 ~ 127
+let uint8_range :UInt8 = 255;                    // 1 byte => 0 ~ 255
+let int32_range :Int32 = -2147483648;            // 4 bytes => -2147483648 ~ 2147483647
+let uint32_range :UInt32 = 4294967295;           // 4 bytes => 0 ~ 4294967295
+let int64_range :Int64 = -9223372036854775808;   // 8 bytes => -9223372036854775808 ~ 9223372036854775807
+let uint64_range :UInt64 = 18446744073709551615; // 8 bytes => 0 ~ 18446744073709551615
+let float_range :Float = 3.4E+38;                // 4 bytes => 1.2E-38 ~ 3.4E+38 (~6 digits)
+let double_range :Double = 1.7E+308;             // 8 bytes => 2.3E-308 ~ 1.7E+308 (~15 digits)
+
 
 
 /* ------------------------------------ print ------------------------------------ */
