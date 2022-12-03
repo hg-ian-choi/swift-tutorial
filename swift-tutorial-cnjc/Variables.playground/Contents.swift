75024eff1dc18cad2831f6edf9386a7602b7ca71
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-03 13:16:08 +0900

    cnjc: try "variables"

---
+++ swift-tutorial-cnjc/Variables.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Cocoa
+
+/* ------------------------------------ declare variable ------------------------------------ */
+// var variableName = <initial value>
+
+var varA = 0;
+print(varA);
+
+var varB: Float;
+varB = 3.14159;
+print(varB);
+
+
+
+/* ------------------------------------ variable naming rules ------------------------------------ */
+var _var = "Hello, Swift!"; // startsWith("_")
+var 안녕 = "안녕, 세계!"; // can naming by korean
+var 你好 = "你好, 世界!"; // alse can use chinese
+print(안녕);
+print(你好);
+
+
+
+/* ------------------------------------ variable output ------------------------------------ */
+var firstName = "IAN";
+var lastName = "CHOI";
+print("\(firstName) \(lastName)");
+
