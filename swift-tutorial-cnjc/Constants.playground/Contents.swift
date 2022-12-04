25cfd930fac76e0cca0d7db1c968f46f1d48df0e
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-04 12:30:45 +0900

    cnjc: try constant

---
+++ swift-tutorial-cnjc/Constants.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Cocoa
+
+/* ------------------------------------ delaration ------------------------------------ */
+// let constantName = <initial value>
+let constA = 42;
+print(constA); // => 42
+
+
+
+/* ------------------------------------ type annotation ------------------------------------ */
+// var constantName:<data type> = <optional initial value>;
+let constB = 42;
+print(constB); // => 42
+
+let constC:Float = 3.14159;
+print(constC); // => 3.14159
+
+
+
+/* ------------------------------------ naming ------------------------------------ */
+let _const = "Hello, World!";
+print(_const);
+let 你好 = "你好，世界!";
+print(你好);
+let 안녕 = "안녕, 세계!";
+print(안녕);
+
+
+
+/* ------------------------------------ output ------------------------------------ */
+let firstName = "IAN";
+let lastName = "CHOI";
+print("\(firstName) \(lastName)");
