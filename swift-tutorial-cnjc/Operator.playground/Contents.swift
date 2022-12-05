b3f4bcb5b502c6109acb1fb0b8c6c81b99c1407c
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-05 09:39:55 +0900

    cnjc: try "arithmetic, comparison, logical, bitwise, assignment" operators

---
+++ swift-tutorial-cnjc/Operator.playground/Contents.swift
@@ -0,0 +1,160 @@
+import Cocoa
+
+/* ------------------------------------ Arithmetic Operator ------------------------------------ */
+/*
+ "+" => addition
+ "-" => subtraction
+ "*" => multiplication
+ "/" => division
+ "%" => remainder
+ 
+ * swift3 removed ++ and --
+ */
+var A = 10;
+var B = 20;
+
+print("A + B => \(A + B)"); // => 30
+print("A - B => \(A - B)"); // => -10
+print("A * B => \(A * B)"); // => 200
+print("B / A => \(B / A)"); // => 2
+A += 1; // the same like A++
+print("A += 1 => \(A)");    // => 11
+B -= 1; // the same like B--
+print("B -= 1 => \(B)");    // => 19
+
+
+
+/* ------------------------------------ Comparison Operator ------------------------------------ */
+/*
+ "==" => equal to
+ "!=" => not equal to
+ ">"  => greater than
+ "<"  => less than
+ ">=" => equal to or greater than
+ "<=" => less than or equal to
+ */
+var C = 10;
+var D = 20;
+
+print("C == D => \(A == B)"); // => false
+print("C != D => \(A != B)"); // => true
+print("C > D => \(A > B)");   // => false
+print("C < D => \(A < B)");   // => true
+print("C >= D => \(A >= B)"); // => false
+print("C <= D => \(A <= B)"); // => true
+
+
+
+/* ------------------------------------ Logical Operator ------------------------------------ */
+/*
+ "&&" => and
+ "||" => or
+ "!" => negation
+ */
+var E = true;
+var F = false;
+
+print("E && F => \(E && F)"); // => false
+print("E || F => \(E || F)"); // => true
+print("!E => \(!E)");         // => false
+print("!F => \(!F)");         // => true
+
+
+
+/* ------------------------------------ Bitwise Operator ------------------------------------ */
+/*
+ Binary Level Operation
+ */
+var p0 = 0;
+var q0 = 0;
+var p1 = 1;
+var q1 = 1;
+// p = 0, q = 0
+print(p0 & q0); // => 0
+print(p0 | q0); // => 0
+print(p0 ^ q0); // => 0
+
+// p = 0, q = 1
+print(p0 & q1); // => 0
+print(p0 | q1); // => 1
+print(p0 ^ q1); // => 1
+
+// p = 1, q = 0
+print(p1 & q0); // => 0
+print(p1 | q0); // => 1
+print(p1 ^ q0); // => 1
+
+// p = 1, q = 1
+print(p1 & q1); // => 1
+print(p1 | q1); // => 1
+print(p1 ^ q1); // => 0
+
+/*
+ "&"  => and
+ "|"  => or
+ "^"  => xor(exclusive OR)
+ "~"  => negation
+ "<<" => left shift
+ ">>" => right shift
+ */
+var G = 60; // binary: 0011 1100
+var H = 13; // binary: 0000 1101
+print("G&H => \(G&H)"); // => 12 => binary: 0000 1100
+print("G|H => \(G|H)"); // => 61 => binary: 0011 1101
+print("G^H => \(G^H)"); // => 49 => binary: 0011 0001
+print("~G => \(~G)");   // => -61 => binary: -0011 1101
+print("~H => \(~H)");   // => -14 => binary: -0000 1110
+
+
+
+/* ------------------------------------ Assignment Operator ------------------------------------ */
+/*
+ "="   => assignment
+ "+="  => assignment after addition
+ "-="  => assignment after subtraction
+ "*="  => assignment after multiplication
+ "/="  => assignment after division
+ "%="  => assignment after remainder
+ "<<=" => assignment after left shift
+ ">>=" => assignment after right shift
+ "&="  => assignment after and
+ "|="  => assignment after or
+ "^="  => assignment after xor
+ */
+var I = 10;
+var J = 20;
+var K = 100;
+
+K = I + J;
+print("K => \(K)");
+
+K += I;
+print("K => \(K)");
+
+K -= I;
+print("K => \(K)");
+
+K *= I;
+print("K => \(K)");
+
+K /= I;
+print("K => \(K)");
+
+K %= I;
+print("K => \(K)");
+
+K <<= I;
+print("K => \(K)");
+
+K >>= I;
+print("K => \(K)");
+
+K &= I;
+print("K => \(K)");
+
+K ^= I;
+print("K => \(K)");
+
+K |= I;
+print("K => \(K)");
+
