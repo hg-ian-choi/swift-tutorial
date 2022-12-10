c3653ccf70072b45a05b7b047e738f5a44d46d21
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-10 23:38:19 +0900

    cnjc: try more "subscripts"

--- swift-tutorial-cnjc/Subscripts.playground/Contents.swift
+++ swift-tutorial-cnjc/Subscripts.playground/Contents.swift
@@ -59,7 +59,39 @@ print(week[1]); // => Monday
 print(week[2]); // => Tuesday
 print(week[3]); // => Wednesday
 
-
+/* ------------------------------------ Subscript Usage ------------------------------------ */
 var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4];
 numberOfLegs["bird"] = 2;
 print(numberOfLegs); // => ["cat": 4, "ant": 6, "spider": 8, "bird": 2]
+
+
+
+/* ------------------------------------ Subscript Options ------------------------------------ */
+struct Matrix {
+    let rows: Int, columns: Int;
+    var print: [Double];
+    init(rows: Int, columns: Int) {
+        self.rows = rows;
+        self.columns = columns;
+        print = Array(repeating: 0.0, count: rows * columns);
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            return print[(row * columns) + column];
+        }
+        set {
+            print[(row * columns) + column] = newValue;
+        }
+    }
+}
+// create a new Matrix with 3 rows and 3 columns
+var mat = Matrix(rows: 3, columns: 3);
+
+mat[0, 0] = 1.0;
+mat[0, 1] = 2.0;
+mat[1, 0] = 3.0;
+mat[1, 1] = 5.0;
+print("mat[0, 0] = \(mat[0, 0])"); // => 1.0
+print("mat[0, 1] = \(mat[0, 1])"); // => 2.0
+print("mat[1, 0] = \(mat[1, 0])"); // => 3.0
+print("mat[1, 1] = \(mat[1, 1])"); // => 5.0
