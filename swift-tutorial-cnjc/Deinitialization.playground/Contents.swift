e09db98526d5700aff8845220e4c5f0ab669be4b
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-13 22:04:59 +0900

    cnjc: try more "Deinitialization"

--- swift-tutorial-cnjc/Deinitialization.playground/Contents.swift
+++ swift-tutorial-cnjc/Deinitialization.playground/Contents.swift
@@ -22,3 +22,30 @@ import Cocoa
      // perform the deinitialization
  }
  */
+
+/*
+ Deinitializers are called automatically, just before instance deallocation takes place.
+ You aren’t allowed to call a deinitializer yourself. Superclass deinitializers are inherited
+ by their subclasses, and the superclass deinitializer is called automatically at the end of
+ a subclass deinitializer implementation. Superclass deinitializers are always called,
+ even if a subclass doesn’t provide its own deinitializer.
+ 
+ Because an instance isn’t deallocated until after its deinitializer is called, a deinitializer
+ can access all properties of the instance it’s called on and can modify its behavior based on
+ those properties (such as looking up the name of a file that needs to be closed).
+ */
+var counter = 0;
+class BaseClass {
+    init() {
+        counter += 1;
+    }
+    deinit {
+        counter -= 1;
+    }
+}
+
+var show: BaseClass? = BaseClass()
+print(counter) // => 1
+show = nil
+print(counter) // => 0
+
