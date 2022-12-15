160d47ccf25218ad645da21e2a4e9081353ad840
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-16 00:51:27 +0900

    cnjc: try more "Extensions"

--- swift-tutorial-cnjc/Extensions.playground/Contents.swift
+++ swift-tutorial-cnjc/Extensions.playground/Contents.swift
@@ -20,3 +20,200 @@ import Cocoa
  NOTE:
     Extensions can add new functionality to a type, but they can’t override existing functionality.
  */
+
+
+
+
+/* ------------------------------------ Extension Syntax ------------------------------------ */
+/*
+ Declare extensions with the extension keyword:
+     extension SomeType {
+         // new functionality to add to SomeType goes here
+     }
+ 
+ An extension can extend an existing type to make it adopt one or more protocols.
+ To add protocol conformance, you write the protocol names the same way as you write them for a class or structure:
+     extension SomeType: SomeProtocol, AnotherProtocol {
+         // implementation of protocol requirements goes here
+     }
+ 
+ Adding protocol conformance in this way is described in Adding Protocol Conformance with an Extension.
+
+ An extension can be used to extend an existing generic type, as described in Extending a Generic Type.
+ You can also extend a generic type to conditionally add functionality, as described in Extensions with a Generic Where Clause.
+ 
+ NOTE:
+    If you define an extension to add new functionality to an existing type,
+    the new functionality will be available on all existing instances of that type,
+    even if they were created before the extension was defined.
+ */
+
+
+
+/* ------------------------------------ Computed Properties ------------------------------------ */
+/*
+ Extensions can add computed instance properties and computed type properties to existing types.
+ */
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+
+let oneInch = 25.4.mm
+print("One inch is \(oneInch) meters") // => One inch is 0.0254 meters
+
+let threeFeet = 3.ft
+print("Three feet is \(threeFeet) meters") // => Three feet is 0.914399970739201 meters"
+
+let aMarathon = 42.km + 195.m
+print("A marathon is \(aMarathon) meters long") // => A marathon is 42195.0 meters long
+
+
+
+/* ------------------------------------ Initializers ------------------------------------ */
+/*
+ Extensions can add new initializers to existing types.
+ This enables you to extend other types to accept your own custom types as initializer parameters,
+ or to provide additional initialization options that were not included as part of the type’s original implementation.
+
+ Extensions can add new convenience initializers to a class,
+ but they can’t add new designated initializers or deinitializers to a class.
+ Designated initializers and deinitializers must always be provided by the original class implementation.
+
+ If you use an extension to add an initializer to a value type that provides default values for all of its stored properties
+ and doesn’t define any custom initializers,
+ you can call the default initializer and memberwise initializer for that value type from within your extension’s initializer.
+ This wouldn’t be the case if you had written the initializer as part of the value type’s original implementation,
+ as described in Initializer Delegation for Value Types.
+
+ If you use an extension to add an initializer to a structure that was declared in another module,
+ the new initializer can’t access self until it calls an initializer from the defining module.
+ 
+ NOTE:
+    If you provide a new initializer with an extension,
+    you are still responsible for making sure that each instance is fully initialized once the initializer completes.
+ */
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+// centerRect's origin is (2.5, 2.5) and its size is (3.0, 3.0)
+
+
+
+/* ------------------------------------ Methods ------------------------------------ */
+/* Extensions can add new instance methods and type methods to existing types. */
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+3.repetitions {
+    print("Hello!")
+}
+
+
+
+/* ------------------------------------ Mutating Instance Methods ------------------------------------ */
+/*
+ Instance methods added with an extension can also modify (or mutate) the instance itself.
+ Structure and enumeration methods that modify self or its properties must mark the instance method as mutating,
+ just like mutating methods from an original implementation.
+ */
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+var someInt = 3
+someInt.square() // someInt is now 9
+
+
+
+/* ------------------------------------ Subscripts ------------------------------------ */
+/* Extensions can add new subscripts to an existing type. */
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+        for _ in 0..<digitIndex {
+            decimalBase *= 10
+        }
+        return (self / decimalBase) % 10
+    }
+}
+746381295[0] // returns 5
+746381295[1] // returns 9
+746381295[2] // returns 2
+746381295[8] // returns 7
+
+746381295[9] // returns 0, as if you had requested:
+0746381295[9]
+
+
+
+/* ------------------------------------ Nested Types ------------------------------------ */
+/* Extensions can add new nested types to existing classes, structures, and enumerations. */
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+func printIntegerKinds(_ numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case .negative:
+            print("- ", terminator: "")
+        case .zero:
+            print("0 ", terminator: "")
+        case .positive:
+            print("+ ", terminator: "")
+        }
+    }
+    print("")
+}
+printIntegerKinds([3, 19, -27, 0, -6, 0, 7]) // => + + - 0 - 0 +
+/*
+ NOTE:
+    number.kind is already known to be of type Int.Kind.
+    Because of this,
+    all of the Int.Kind case values can be written in shorthand form inside the switch statement,
+    such as .negative rather than Int.Kind.negative.
+ */
