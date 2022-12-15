8bef61786cf0780b86547b0b7c7710e234e14572
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-16 00:39:11 +0900

    cnjc: try "Extensions"

---
+++ swift-tutorial-cnjc/Extensions.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+/* ------------------------------------ Extensions ------------------------------------ */
+/*
+ Extensions add new functionality to an existing class, structure, enumeration, or protocol type.
+ This includes the ability to extend types for which you don’t have access to the original source code (known as retroactive modeling).
+ Extensions are similar to categories in Objective-C. (Unlike Objective-C categories, Swift extensions don’t have names.)
+ 
+ Extensions in Swift can:
+    1. Add computed instance properties and computed type properties
+    2. Define instance methods and type methods
+    3. Provide new initializers
+    4. Define subscripts
+    5. Define and use new nested types
+    6. Make an existing type conform to a protocol
+
+ In Swift, you can even extend a protocol to provide implementations of its requirements
+ or add additional functionality that conforming types can take advantage of.
+ 
+ NOTE:
+    Extensions can add new functionality to a type, but they can’t override existing functionality.
+ */
