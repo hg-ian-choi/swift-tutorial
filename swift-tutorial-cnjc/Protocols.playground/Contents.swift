8289af3f2f111adfd9f393d5e29018a8973032c2
hg-ian-choi <hg.ian.choi@gmail.com>
2022-12-19 23:38:11 +0900

    cnjc: try more "Protocols"

--- swift-tutorial-cnjc/Protocols.playground/Contents.swift
+++ swift-tutorial-cnjc/Protocols.playground/Contents.swift
@@ -160,3 +160,69 @@ enum OnOffSwitch: Togglable {
 var lightSwitch = OnOffSwitch.off
 lightSwitch.toggle()
 print("lightSwitch is now equal to .\(lightSwitch)") // => lightSwitch is now equal to .on
+
+
+
+/*------------------------------------ Initializer Requirements ------------------------------------ */
+/*
+ Protocols can require specific initializers to be implemented by conforming types.
+ You write these initializers as part of the protocol’s definition in exactly the same way as for normal initializers,
+ but without curly braces or an initializer body:
+ */
+protocol SomeProtocol {
+    init(someParameter: Int)
+}
+
+/* Class Implementations of Protocol Initializer Requirements */
+/*
+ You can implement a protocol initializer requirement on a conforming class as either
+ a designated initializer or a convenience initializer.
+ In both cases, you must mark the initializer implementation with the required modifier:
+ */
+class SomeClass: SomeProtocol {
+    required init(someParameter: Int) {
+        // initializer implementation goes here
+    }
+}
+
+/*
+ The use of the required modifier ensures that you provide an explicit or inherited implementation of the
+ initializer requirement on all subclasses of the conforming class, such that they also conform to the protocol.
+ 
+ NOTE:
+     You don’t need to mark protocol initializer implementations with the required modifier on classes
+     that are marked with the final modifier, because final classes can’t subclassed.
+     For more about the final modifier, see Preventing Overrides.
+ */
+
+
+/*
+ If a subclass overrides a designated initializer from a superclass,
+ and also implements a matching initializer requirement from a protocol,
+ mark the initializer implementation with both the required and override modifiers:
+ */
+protocol SomeProtocolA {
+    init()
+}
+
+class SomeSuperClassA {
+    init() {
+        // initializer implementation goes here
+    }
+}
+
+class SomeSubClassA: SomeSuperClassA, SomeProtocolA {
+    // "required" from SomeProtocol conformance; "override" from SomeSuperClass
+    required override init() {
+        // initializer implementation goes here
+    }
+}
+
+
+/* Failable Initializer Requirements */
+/*
+ Protocols can define failable initializer requirements for conforming types, as defined in Failable Initializers.
+
+ A failable initializer requirement can be satisfied by a failable or nonfailable initializer on a conforming type.
+ A nonfailable initializer requirement can be satisfied by a nonfailable initializer or an implicitly unwrapped failable initializer.
+ */
